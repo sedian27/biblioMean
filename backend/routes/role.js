49487d801fb5397e3613b746799e43197aa0cc74
sedian <diestrasergio@gmail.com>
2022-02-18 15:34:08 -0500

    roleMdd added

--- backend/routes/role.js
+++ backend/routes/role.js
@@ -1,9 +1,10 @@
 import express from "express";
 import controller from "../controllers/role.js";
+import roleMdd from "../middleware/roleValidations.js";
 
 const router = express.Router();
 
-router.post("/registerRole", controller.validations, controller.registerRole);
+router.post("/registerRole", roleMdd.validations, controller.registerRole);
 router.get("/listRole/:name?", controller.listRole);
 
 export default router;
