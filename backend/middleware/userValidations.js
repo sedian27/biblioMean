8b500311bf5f128f204447dde81ad9e7f4dcf0b0
sedian <diestrasergio@gmail.com>
2022-03-02 09:21:25 -0500

    space added

--- backend/middleware/userValidations.js
+++ backend/middleware/userValidations.js
@@ -3,6 +3,7 @@ import model from "../models/user.js";
 const validData = (req, res, next) => {
   if (!req.body.name || !req.body.email || !req.body.password)
     return res.status(400).send({ message: "Incomplete Data" });
+
   next();
 };
 
